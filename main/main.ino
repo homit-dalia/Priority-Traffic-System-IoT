537ae40ad17e351dfa35d7dba8133922b8156cf8
Homit Dalia <homitdalia59@gmail.com>
2023-05-12 03:08:46 +0530

    Sound Sensor code added

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,5 @@
 int SOUND = 1;
-
+int SOUNDANALOG = A4;
 int button1 = 2;
 int R1 = 3;
 int G1 = 4;
@@ -16,19 +16,23 @@ int button4 = 8;
 int R4 = 9;
 int G4 = 10;
 
-int delay_red = 2000;
 int delay_green = 2000;
 
-int flash_time = 150; // time is double
+
+int flash_time = 300; // time is double
+int siren_threshold = 800;
 
 int buttonState = 0;
 int buttonState1 = 0;
 
+
 int button_prev_state[4];
 int button_pressed_count[4];
 
+
 void setup() {
   pinMode(SOUND, INPUT);
+  pinMode(SOUNDANALOG, INPUT);
 
   pinMode(button1, INPUT);
   pinMode(R1, OUTPUT);
@@ -45,82 +49,80 @@ void setup() {
   pinMode(button4, INPUT);
   pinMode(R4, OUTPUT);
   pinMode(G4, OUTPUT);
-    
+
   digitalWrite(R1, HIGH);
   digitalWrite(R2, HIGH);
   digitalWrite(R3, HIGH);
   digitalWrite(R4, HIGH);
 
   delay(1000);
+  Serial.begin(9600);
 }
 
 void loop() {
-//   buttonState = digitalRead(button3);
-//   buttonState1 = digitalRead(button1);
 
-//   if (buttonState != HIGH) {
-//     // turn LED on:
-//     digitalWrite(G3, HIGH);
-//   } else {
-//     // turn LED off:
-//     digitalWrite(G3, LOW);
-//   }
-
-// if (buttonState1 != HIGH) {
-//     // turn LED on:
-//     digitalWrite(G1, HIGH);
-//   } else {
-//     // turn LED off:
-//     digitalWrite(G1, LOW);
-//   }
 
   green_light(G1, R1);
   other_lights_red(R1);
+
   if(button_pressed(button1, 0)){flash_red_green(R1);}
-  // flash_red_green(R1);
-  // ambulance_detected(G1);
+  siren_sound();    //final change was to replace siren sound after button pressed. Replace if the code does not function
   delay(delay_green);
+  siren_sound();
   green_off(G1);
 
   green_light(G2, R2);
   other_lights_red(R2);
+
   if(button_pressed(button2, 1)){flash_red_green(R2);}
+  siren_sound();
   delay(delay_green);
+  siren_sound();
   green_off(G2);
 
   green_light(G3, R3);
   other_lights_red(R3);
+
   if(button_pressed(button3, 2)){flash_red_green(R3);}
+  siren_sound();
   delay(delay_green);
+  siren_sound();
   green_off(G3);
 
   green_light(G4, R4);
   other_lights_red(R4);
+
   if(button_pressed(button4, 3)){flash_red_green(R4);}
+  siren_sound(); 
   delay(delay_green);
+  siren_sound();
   green_off(G4);
 
 }
 
-bool siren_sound(){
-
+void siren_sound(){
+  for(int i=0; i<50; i++){
+    //Serial.println(analogRead(SOUNDANALOG));
+    if(analogRead(SOUNDANALOG) > siren_threshold){
+            ambulance_detected(G1);
+            digitalWrite(G1,HIGH);
+            delay(1000);
+            digitalWrite(G1,LOW);
+            break;
+    }
+  delay(50);
+  }
 }
-
-// void flash_all_red_lights(int dont_flash){
-//   int lights[3];
-//   int j=0
-//   for (int i=1; i < 4;i++){
-//       if(dont_flash != i){
-//         lights[j] = i
-//       }
-//   }
-//   for(int i=0; i< 50; i++){
-//     digitalWrite(red, HIGH);
-//   }
-// }
-
+ 
 void ambulance_detected(int green){
   digitalWrite(green, HIGH);
+  int array[4] = {G1, G2, G3, G4};
+
+  for(int i =0; i<4; i++){
+    if(array[i] != green){
+      digitalWrite(array[i], LOW);
+    }
+  }
   for(int i=0; i< 50; i++){
     digitalWrite(R1, HIGH);
     digitalWrite(R2, HIGH);
@@ -141,20 +143,18 @@ void ambulance_detected(int green){
 
 bool button_pressed(int pin, int value){
   buttonState = digitalRead(pin);
-  if (buttonState != button_prev_state[value]){
-
+  if (buttonState == HIGH){
       button_pressed_count[value]++;
       Serial.println("Button");
       Serial.println(button_pressed_count[value]);
       return true;
-    
   }
   return false;
 }
 
 void flash_red_green(int red){
-  
-  for(int i=0; i<30; i++){
+
+  for(int i=0; i<15; i++){
     digitalWrite(red, HIGH);
     delay(flash_time);
     digitalWrite(red, LOW);
